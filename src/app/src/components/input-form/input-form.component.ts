6bb6c5350073d4f6ad139f9cff263debcad20a3e
Anderson E. Peñaloza C <designeromega@gmail.com>
2021-06-29 22:01:35 +0200

    Finished Input data form

--- src/app/src/components/input-form/input-form.component.ts
+++ src/app/src/components/input-form/input-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-input-form',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-form.component.scss']
 })
 export class InputFormComponent implements OnInit {
+  inputDataForm: any;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
+    this.inputDataForm = this.fb.group({
+      areaWidth: [null, Validators.required],
+      areaHeight: [null, Validators.required],
+      roverStartX: [null, Validators.required],
+      roverStartY: [null, Validators.required],
+      roverOrders: [
+        null, [
+          Validators.required,
+          Validators.pattern(/[NSWEnswe]{1,}$/)
+        ]
+      ],
+    });
   }
-
+  public hasError = (controlName: string, errorName: string) => {
+    return this.inputDataForm.controls[controlName].hasError(errorName);
+  }
 }
--- src/app/src/components/input-form/input-form.module.ts
+++ src/app/src/components/input-form/input-form.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputFormComponent } from './input-form.component';
 import { InputFormRoutingModule } from './input-form-routing.module';
+import { AngularMaterialModule } from '../../../angular-material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     InputFormComponent
   ],
   imports: [
+		AngularMaterialModule,
     CommonModule,
+    FormsModule,
+		ReactiveFormsModule,
     InputFormRoutingModule
   ]
 })
